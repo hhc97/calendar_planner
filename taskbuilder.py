849e154e7f7e8de4f93476e1b47102d2e7dd294c
Haocheng Hu <hc.hu@utoronto.ca>
2020-03-30 18:12:11 -0400

    add Task class to TaskBuilder

--- taskbuilder.py
+++ taskbuilder.py
@@ -1,4 +1,53 @@
-from task import Task
+from datetime import datetime, timedelta
+
+
+class Task:
+    """
+    Args:
+        name(str): the name of the task
+        begin(datetime, or str): the begin time of the task.
+                If it is str, it is in "09/19/18 13:55:26" format.
+                If begin is given, the task's flexibility is set to False and
+                priority set to 4.
+        end(datetime, or str): the end time of the task. Same format as begin.
+        priority (int): a value in [0, 1, 2, 3, 4].
+        hours (float): will be rounded to the nearest 0.5
+    """
+
+    def __init__(self, name, begin=None, end=None, priority=0, hours=1.0):
+        self.flexible = True
+        self.name = name
+        self.begin = None
+        self.end = None
+        self.hours = round(2 * hours) / 2
+        self.priority = priority
+
+        if begin is not None:
+            if not isinstance(begin, datetime):
+                begin = datetime.strptime(begin, '%m/%d/%y %H:%M:%S')
+            self.begin = begin
+            self.flexible = False
+
+            self.priority = 4
+
+            if end is not None:
+                if not isinstance(end, datetime):
+                    end = datetime.strptime(end, '%m/%d/%y %H:%M:%S')
+                self.end = end
+                delta = self.end - self.begin
+                self.hours = delta.days * 24 + round(2 * delta.seconds / 3600) / 2
+            else:
+                self.end = begin + timedelta(hours=hours)
+
+        if self.flexible:
+            assert priority in range(5), "priority value exceeds the range"
+            self.priority = priority
+            self.hours = hours
+
+    def __str__(self):
+        return f"Name: {self.name}\nFlexible: {self.flexible}\n" \
+            f"Begin: {self.begin}\nEnd: {self.end}\nHours: {self.hours} hours\n" \
+            f"Priority: {self.priority}"
 
 
 class TaskBuilder:
@@ -50,7 +99,6 @@ class TaskBuilder:
     # methods to set attributes, these may be chained together
     # as of now there is no error checking, if you call this method
     # it is assumed that the input is valid
-
     def set_name(self, name: str):
         self.name = name
         return self
@@ -71,9 +119,12 @@ class TaskBuilder:
 
     def build(self):
         """Builds the current configuration of Task and returns it"""
-        ret = Task(self.name, self.begin, self.end, self.priority, self.hours)
-        self.reset()  # resets the current configuration
-        return ret
+        if self.name and self.hours:
+            ret = Task(self.name, self.begin, self.end, self.priority, self.hours)
+            self.reset()  # resets the current configuration
+            return ret
+        else:
+            print('Either name or hours not filled in.')
 
     def reset(self):
         """Resets the attributes for building the next Task"""
